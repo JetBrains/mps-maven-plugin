a623470d4a1fbfc82b678edd1681057794fd5cc0
Sergej Koščejev <sergej@koscejev.cz>
2016-10-16 15:38:00 +0200

    Reformat MavenLogAppender

--- mps-launcher/src/main/java/org/jetbrains/mps/mavenplugin/mps/MavenLogAppender.java
+++ mps-launcher/src/main/java/org/jetbrains/mps/mavenplugin/mps/MavenLogAppender.java
@@ -3,58 +3,55 @@ package org.jetbrains.mps.mavenplugin.mps;
 import org.apache.log4j.AppenderSkeleton;
 import org.apache.log4j.Level;
 import org.apache.log4j.spi.LoggingEvent;
-import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.logging.Log;
 
 public class MavenLogAppender extends AppenderSkeleton {
-	
-	private static Log mavenLog;
+
+    private static Log mavenLog;
 
     public static void startPluginLog(Log mavenLog) {
-	    MavenLogAppender.mavenLog = mavenLog;
+        MavenLogAppender.mavenLog = mavenLog;
     }
 
     public static void endPluginLog() {
-		mavenLog = null;
-	}
+        mavenLog = null;
+    }
 
-	protected void append(LoggingEvent event) {
-		if (mavenLog == null) {
-			return;
-		}
-		Level level = event.getLevel();
-		if (Level.DEBUG.equals(level) && !(mavenLog.isDebugEnabled())) {
-			return;
-		}
+    protected void append(LoggingEvent event) {
+        if (mavenLog == null) {
+            return;
+        }
+        Level level = event.getLevel();
+        if (Level.DEBUG.equals(level) && !(mavenLog.isDebugEnabled())) {
+            return;
+        }
 
-		String text = this.layout.format(event);
-		Throwable throwable = null;
-		if (event.getThrowableInformation() != null) {
-			throwable = event.getThrowableInformation().getThrowable();
-		}
-		
-		if (Level.DEBUG.equals(level) || Level.TRACE.equals(level)) {
-			mavenLog.debug(text, throwable);
-		} else if (Level.INFO.equals(level)) {
-			mavenLog.info(text, throwable);
-		} else if (Level.WARN.equals(level)) {
-			mavenLog.warn(text, throwable);
-		} else if (Level.ERROR.equals(level) || Level.FATAL.equals(level)) {
-			mavenLog.error(text, throwable);
-		} else {
-			mavenLog.error(text, throwable);
-		}
-	}
+        String text = this.layout.format(event);
+        Throwable throwable = null;
+        if (event.getThrowableInformation() != null) {
+            throwable = event.getThrowableInformation().getThrowable();
+        }
 
-	public void close() {
-		mavenLog = null;
-	}
+        if (Level.DEBUG.equals(level) || Level.TRACE.equals(level)) {
+            mavenLog.debug(text, throwable);
+        } else if (Level.INFO.equals(level)) {
+            mavenLog.info(text, throwable);
+        } else if (Level.WARN.equals(level)) {
+            mavenLog.warn(text, throwable);
+        } else if (Level.ERROR.equals(level) || Level.FATAL.equals(level)) {
+            mavenLog.error(text, throwable);
+        } else {
+            mavenLog.error(text, throwable);
+        }
+    }
 
-	public boolean requiresLayout() {
-		return true;
-	}
+    public void close() {
+        mavenLog = null;
+    }
 
-	
-	
-	
+    public boolean requiresLayout() {
+        return true;
+    }
+
+
 }
